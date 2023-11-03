a84881e1039ef993455ea3eb44bbf0cab94013d2
JESSE YEBOAH OWUSU <jesse.yeboah@innorik.com>
2023-11-03 09:07:46 +0000

    excercise

--- app/src/component/Button/button.tsx
+++ app/src/component/Button/button.tsx
@@ -1,5 +1,5 @@
 export const Button = () => {
     return (<div >
-        <button className="bg-green-400 p-2 rounded-lg text-white">Edit User</button>
+        <button className="bg-green-400 p-2 rounded-lg text-white" style={{marginLeft:'350px'}}>Edit User</button>
     </div>)
 }
--- app/src/component/Container/container.tsx
+++ app/src/component/Container/container.tsx
@@ -3,9 +3,10 @@ import { Userinfo } from "../UserInfo/userinfo"
 export const Container = ()=>{
     return (
     <>
-    <div className="justify-center shadow-lg p-8 rounded-lg shadow-lg bg-white m-12 flex ">
+    <div className="justify-center shadow-lg p-8 py-10 rounded-lg shadow-lg bg-white m-12 flex ">
         <Userinfo/>
         <Button/>
+        <hr className=""/>
     </div>
     </>
     )
