967ebf470ac41bf56011baa078662cb051bbedcb
eaglementality <jesseowusuyeboah99@gmail.com>
2023-11-04 20:28:30 +0000

    Task management system

---
+++ app/src/component/Counts.tsx
@@ -0,0 +1,22 @@
+export const Counts = ()=>{
+    return (<>
+    <div className="justify-center py-8 bg-white px-12 rounded-lg shadow-lg">
+        <div className="p-8 m-8 ronded-lg shadow-lg text-center"> 
+            <div className="text-xl py-2">Todo</div>
+            <div className="font-bold text-3xl">6</div>
+        </div>
+        <div className="p-8 m-8 ronded-lg shadow-lg text-center"> 
+            <div className="text-xl py-2">In Progress</div>
+            <div className="font-bold text-3xl">4</div>
+        </div>
+        <div className="p-8 m-8  ronded-lg shadow-lg text-center"> 
+            <div className="text-xl py-2">Review</div>
+            <div className="font-bold text-3xl">2</div>
+        </div>
+        <div className="p-8 m-8 ronded-lg shadow-lg text-center"> 
+            <div className="text-xl py-2">Done</div>
+            <div className="font-bold text-3xl">2</div>
+        </div>
+    </div>
+    </>)
+}
--- app/src/component/TaskBoard/container.jsx
+++ app/src/component/TaskBoard/container.jsx
@@ -1,18 +1,25 @@
-import { AddTask } from "../AddandEditTask/addTask"
+import { AddTask } from "../AddTask/addTask"
+import { Counts } from "../Counts"
 import { DoneList } from "../DoneList/Donelist"
 import { ProgressList } from "../ProgressList/progressList"
+import { SildeBar } from "../Sidebar"
 import { TaskList } from "../TaskList/TaskList"
 
 export const Container = () => {
 return (
 <main className="flex flex-col h-screen w-screen">
-<div className="bg-white rounded-lg m-5 ml-10 mr-8 shadow-lg p-5">
-<h1 className="font-bold text-3xl m-5">Tasks</h1>
-<div className=""><AddTask/></div>
-<div className="flex bg-gray-200 justify-center rounded-lg p-5">
-    <TaskList/>
-    <ProgressList/>
-    <DoneList/>
+<div className="bg-gray-700 rounded-lg m-5 ml-10 mr-8 shadow-lg p-5">
+<h1 className="font-bold text-white text-3xl m-2">Tasks Board</h1>
+<hr className="m-3"/>
+{/* <div className=""><AddTask/></div> */}
+<div className="flex justify-center gap-8">
+    <SildeBar/>
+    <div className="flex bg-white justify-center rounded-lg shadow-lg p-5">
+        <TaskList/>
+        <ProgressList/>
+        <DoneList/>
+    </div>
+    <Counts/>
 </div>
 </div>
 </main>)
