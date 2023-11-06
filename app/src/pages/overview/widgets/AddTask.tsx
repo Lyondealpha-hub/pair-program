0b9127327df83874909be32aa5753518ee6fa3f7
Oct Oscar <71621912+6spiritman@users.noreply.github.com>
2023-11-06 14:41:19 +0000

    update: replace input with select

--- app/src/pages/overview/widgets/AddTask.tsx
+++ app/src/pages/overview/widgets/AddTask.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AddCategories } from './AddCategories'
 import { SubmitHandler, useForm } from 'react-hook-form'
-import useTodolist from '../../../lib/context/context'
+import useTodolist, { categoryItemProp } from '../../../lib/context/context'
 
 type Inputs = {
   title: string,
@@ -13,9 +13,9 @@ type Inputs = {
 
 export const AddTask = () => {
 
-  const {setTodoItems, todoItems} = useTodolist()
+  const {setTodoItems, todoItems, categoryItems} = useTodolist()
 
-  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>(
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>(
       {
             defaultValues: {
             title: '',
@@ -41,36 +41,43 @@ export const AddTask = () => {
     })
 
 
-    console.log(todoItems)
+    reset();
 
   }
 
 
   return (
-    <main className='flex px-8 py-10 rounded-xl bg-stone-50 flex-1'>
+    <main className='flex px-2 py-2 rounded-xl bg-stone-50 flex-1'>
 
       <AddCategories/>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="px-6 flex flex-col mt-8 space-y-6">
+      <form onSubmit={handleSubmit(onSubmit)} className="px-2 flex flex-col mt-8 space-y-6">
         <section className="flex flex-col space-y-3">
 
           <div className="flex flex-col space-y-1.5">
             <label className='text-sm font-medium text-slate-700 tracking-tight' htmlFor="">{'Title'}</label>
             <input type="text" className="bg-stone-100 focus:bg-white focus:border focus:border-blue-300 text-slate-400 font-medium text-base px-6 py-2 rounded-xl outline-none border placeholder-slate-300" placeholder='Enter Task Title' {...register("title", { required: true })} />
-            {errors.title && <span className="text-red-500 text-sm">This field is required</span>}
+            {errors.title && <span className="text-red-500 text-sm">Title is required</span>}
           </div>
 
           <div className="flex flex-col space-y-1.5">
             <label className='text-sm font-medium text-slate-700 tracking-tight' htmlFor="">{'Due Date'}</label>
             <input type="date" className="bg-stone-100 text-slate-400 font-medium text-base px-6 py-2 rounded-xl outline-none border" {...register("dueDate", { required: true })} />
-            {errors.dueDate && <span className="text-red-500 text-sm">This field is required</span>}
+            {errors.dueDate && <span className="text-red-500 text-sm">Date is required</span>}
           </div>
+
 
           <div className="flex flex-col space-y-1.5">
             <label className='text-sm font-medium text-slate-700 tracking-tight' htmlFor="">{'Category'}</label>
-            <input type="text" className="bg-stone-100 focus:bg-white focus:border focus:border-blue-300 text-slate-400 font-medium text-base px-6 py-2 rounded-xl outline-none border" placeholder='Type Category' {...register("category", { required: true })} />
-            {errors.category && <span className="text-red-500 text-sm">This field is required</span>}
+            <select className="bg-stone-100 focus:bg-white focus:border focus:border-blue-300 text-slate-400 font-medium text-base px-6 py-2 rounded-xl outline-none border" {...register("category", { required: true })}>
+              <option value="" disabled selected>Select Category</option>
+              {categoryItems.categories.map((item:categoryItemProp) => (
+                <option value={item.name}>{item.name}</option>
+              ))}
+            </select>
+            {errors.category && <span className="text-red-500 text-sm">Category field is required</span>}
           </div>
+          
         </section>
 
         
