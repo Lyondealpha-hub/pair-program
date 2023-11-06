213c5c1e923250d048b47d5e20ef50bb371b8d15
JESSE YEBOAH OWUSU <jesse.yeboah@innorik.com>
2023-11-06 13:12:20 +0000

    still updating

--- app/src/Context/context.tsx
+++ app/src/Context/context.tsx
@@ -1,21 +1,18 @@
-import { createContext,useContext,useState } from "react";
-import data from '../data/data.json'
+import { createContext, useContext, useState } from "react";
+import data from "../data/data.json";
 
-export const Data  = createContext<any>(null);
+export const Data = createContext<any>(null);
 
-export const DataProvider = ({children}: any)=>{
-    
-    const [task, setTask] = useState(()=>data);
-    
-    return (<>
-        <Data.Provider value={{task,setTask}}>
-            {children}
-        </Data.Provider>
-    </>)
-}
+export const DataProvider = ({ children }: any) => {
+  const [task, setTask] = useState(()=>(data));
 
-export const useData = ()=>{
-    return useContext(Data);
-}
+  return (
+    <>
+      <Data.Provider value={{ task, setTask }}>{children}</Data.Provider>
+    </>
+  );
+};
 
-
+export const useData = () => {
+  return useContext(Data);
+};
