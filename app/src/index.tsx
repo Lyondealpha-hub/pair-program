f426f3d347296e3fb07f36d70a7190a2576c515b
Oct Oscar <71621912+6spiritman@users.noreply.github.com>
2023-11-07 14:43:09 +0000

    update: add strict mode to project

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -8,9 +8,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <React.StrictMode>
     <TodolistContext>
       <App />
     </TodolistContext>
+  </React.StrictMode>
+
 );
 
 
